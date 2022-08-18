e340811f95d94aff55940797cbb2b5179cbd07bf
ZiynatI <zisomurodova@gmail.com>
2022-08-18 09:17:31 +0500

    SinePlot updated

--- src/codewars/SinePlot.java
+++ src/codewars/SinePlot.java
@@ -16,6 +16,7 @@ public class SinePlot {
     public static void main(String[] args) {
         printSineSlotVertical(findValuesOfSin(15));
         printSineSlotHorizontal(findValuesOfSin(15));
+        plotSinHorSimple();
     }
 
     public static List<Double> findValuesOfSin(int n) {
@@ -27,9 +28,10 @@ public class SinePlot {
     }
 
     public static void printSineSlotHorizontal(List<Double> valuesOfSin) {
-        for (double y = -1.0; y < 1.0; y += 0.1) {
+        double plusY = 0.1;
+        for (double y = 1.0; y > -1.0; y -= plusY) {
             for (Double valueOfSin : valuesOfSin) {
-                if (y <= valueOfSin && valueOfSin < y + 0.1) {
+                if (y <= valueOfSin && valueOfSin < y + plusY) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
@@ -40,10 +42,42 @@ public class SinePlot {
     }
 
     public static void printSineSlotVertical(List<Double> valuesOfSin) {
+        double plusY = 0.1;
         for (Double valueOfSin : valuesOfSin) {
-            for (double y = -1.0; y < 1.0; y += 0.1) {
-                if (y <= valueOfSin && valueOfSin < y + 0.1) {
+            for (double y = -1.0; y < 1.0; y += plusY) {
+                if (y <= valueOfSin && valueOfSin < y + plusY) {
                     System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void plotSinHorSimple() {
+        int xIntervals = 200;
+        int yIntervals = 20;
+
+        double xMin = 0;
+        double xMax = 4 * Math.PI;
+        double xStep = (xMax - xMin) / xIntervals;
+
+        List<Double> values = new ArrayList<>();
+        for (double x = xMin; x <= xMax; x += xStep) {
+            values.add(Math.sin(x));
+        }
+
+        double yMin = -1.0;
+        double yMax = 1.0;
+        double yStep = (yMax - yMin) / yIntervals;
+
+        for (double y = yMax; y >= yMin; y -= yStep) {
+            for (double value : values) {
+                if (value <= y && value >= (y - yStep)) {
+                    System.out.print("*");
+                } else if (0 <= y && 0 >= (y - yStep)) {
+                    System.out.print("-");
                 } else {
                     System.out.print(" ");
                 }
---
+++ src/codewars/sumOfMaxContiguousSubsequence.java
@@ -0,0 +1,22 @@
+package codewars;
+
+public class sumOfMaxContiguousSubsequence {
+    //The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+    //Max.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+    //// should be 6: {4, -1, 2, 1}
+    //Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+    //Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+    public static void main(String[] args) {
+
+    }
+
+    public static int sequence(int[] arr) {
+        int maxContiguousSubsequence = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for(int j=0;i< arr.length;i++){
+
+            }
+        }
+        return 4;
+    }
+}
