9ad4cc4e917d60ad1eead7aa09a523720c39a083
ZiynatI <zisomurodova@gmail.com>
2022-10-08 23:09:02 +0500

    NumberGuessingGame updated

---
+++ src/codewars/WriteNumberInExpandedForm.java
@@ -0,0 +1,33 @@
+package codewars;
+
+public class WriteNumberInExpandedForm {
+    //https://www.codewars.com/kata/5842df8ccbd22792a4000245
+    //Write Number in Expanded Form
+    //You will be given a number, and you will need to return it as a string in Expanded Form. For example:
+    //Kata.expandedForm(12); # Should return "10 + 2"
+    //Kata.expandedForm(42); # Should return "40 + 2"
+    //Kata.expandedForm(70304); # Should return "70000 + 300 + 4"
+    //NOTE: All numbers will be whole numbers greater than 0.
+    public static void main(String[] args) {
+        System.out.println(expandedForm(42));
+    }
+
+    public static String expandedForm(int num) {
+        return recurse(num, 0, "");
+    }
+
+    public static String recurse(int num, int zeros, String expending) {
+        if (num == 0) {
+            return expending;
+        } else {
+            int tail = num % 10;
+            if (tail == 0) {
+                recurse((num - num % 10) / 10, zeros + 1, expending);
+            } else {
+                recurse((num - num % 10) / 10, zeros + 1, expending + "+" + tail * (int) Math.pow(10, zeros));
+            }
+
+        }
+        return expending;
+    }
+}
--- src/org/helloworld/demo/SwingDemo.java
+++ src/org/helloworld/demo/SwingDemo.java
@@ -1,42 +1,40 @@
 package org.helloworld.demo;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
 
 
 public class SwingDemo extends JFrame {
-    public static void main(String[] args) {
-        SwingDemo app = new SwingDemo();
-        app.init();
-    }
-
-    private boolean isHello = true;
-    private MyForm form;
-
-    private void init() {
-        form = new MyForm();
-        form.btn.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                helloClicked();
-            }
-        });
-
-        this.setMinimumSize(new Dimension(300, 300));
-        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        this.setContentPane(form.formPanel);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-    }
-
-    private void helloClicked() {
-        if (isHello) {
-            form.btn.setText("World!");
-        } else {
-            form.btn.setText("Hello!");
-        }
-        isHello = !isHello;
-    }
+//    public static void main(String[] args) {
+//        SwingDemo app = new SwingDemo();
+//        app.init();
+//    }
+//
+//    private boolean isHello = true;
+//    private MyForm form;
+//
+//    private void init() {
+//        form = new MyForm();
+//        form.btn.addMouseListener(new MouseAdapter() {
+//            @Override
+//            public void mouseReleased(MouseEvent e) {
+//                helloClicked();
+//            }
+//        });
+//
+//        this.setMinimumSize(new Dimension(300, 300));
+//        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+//        this.setContentPane(form.formPanel);
+//        this.setLocationRelativeTo(null);
+//        this.setVisible(true);
+//    }
+//
+//    private void helloClicked() {
+//        if (isHello) {
+//            form.btn.setText("World!");
+//        } else {
+//            form.btn.setText("Hello!");
+//        }
+//        isHello = !isHello;
+//    }
 }
