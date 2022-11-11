2f33032599865c264e92e220e4baae4ca0e66e1b
ZiynatI <zisomurodova@gmail.com>
2022-11-11 21:38:03 +0500

    MyFirstServer updated

--- src/org/helloworld/demo/json/io/MyFirstServer.java
+++ src/org/helloworld/demo/json/io/MyFirstServer.java
@@ -7,6 +7,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class MyFirstServer {
     public static void main(String args[]) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(80)) {
@@ -17,16 +19,21 @@ public class MyFirstServer {
                 System.out.println("Связь  установлена!");
                 byte[] buffer = new byte[2048];
                 int length;
+                System.out.println("Request: ");
                 while ((length = in.read(buffer)) > 0) {
+                    String request = new String(buffer, 0, length, UTF_8);
+                    System.out.println(request);
                     if (in.available() <= 0) {
                         break;
                     }
                 }
+                String helloWorld = "Hello, World!";
+                String response = "Content-Length: " + helloWorld.length() + "Content-Type: text/html" +
+                        "\n\n";
+
                 out.write("HTTP/1.1 200 OK\n".getBytes(StandardCharsets.UTF_8));
-                out.write(("Content-Length: 0\n" +
-                        "Content-Type: text/html" +
-                        "\n\n").getBytes(StandardCharsets.UTF_8));
-                System.out.println("Hello, World!");
+                out.write(response.getBytes(StandardCharsets.UTF_8));
+                out.write(helloWorld.getBytes(StandardCharsets.UTF_8));
             }
         }
     }
