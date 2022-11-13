232c19c5fa389e5ec72a12ff4738d1785089bd74
ZiynatI <zisomurodova@gmail.com>
2022-11-13 20:41:08 +0500

    MyFirstServer updated

--- src/org/helloworld/demo/json/io/MyFirstServer.java
+++ src/org/helloworld/demo/json/io/MyFirstServer.java
@@ -28,13 +28,13 @@ public class MyFirstServer {
                         }
                     }
                     System.out.println(request);
+                    File parentFile = new File("C:/Users/user/Desktop/html");
                     File file;
                     System.out.println("Связь  установлена!");
                     if (request.charAt(5) == ' ') {
-                        file = new File("C:/Users/user/Desktop/html/index.html");
+                        file = new File(parentFile, "index.html");
                     } else {
-                        String filePath = "C:/Users/user/Desktop/html/" + request.substring(5, request.indexOf(" ", 6));
-                        file = new File(filePath);
+                        file = new File(parentFile,request.substring(5, request.indexOf(" ", 6)));
                     }
                     serveConnection(socket, file);
                 }
@@ -43,18 +43,22 @@ public class MyFirstServer {
     }
 
     public static void serveConnection(Socket socket, File file) throws IOException {
-        try (OutputStream out = socket.getOutputStream();
-             FileInputStream fis = new FileInputStream(file)) {
-            byte[] fileToBytes = fis.readAllBytes();
+        try (OutputStream out = socket.getOutputStream()) {
             String r = "HTTP/1.1 200 OK" +
-                    "Content-Length: " + fileToBytes.length + "\n" +
+                    "Content-Length: " + readFile(file).length + "\n" +
                     "Content-Type: text/html";
             String response = r + "\n\n";
             out.write(response.getBytes(UTF_8));
             System.out.println(response);
-            out.write(fileToBytes);
+            out.write(readFile(file));
         }
+    }
 
-
+    public static byte[] readFile(File file) throws IOException {
+        byte[] fileToBytes;
+        try (FileInputStream fis = new FileInputStream(file)) {
+            fileToBytes = fis.readAllBytes();
+        }
+        return fileToBytes;
     }
 }
