3b5c068897618479359b8a3d4ecc48cb414a7cb2
ZiynatI <zisomurodova@gmail.com>
2022-11-13 20:18:03 +0500

    MyFirstServer updated

--- src/org/helloworld/demo/json/io/MyFirstServer.java
+++ src/org/helloworld/demo/json/io/MyFirstServer.java
@@ -3,7 +3,6 @@ package org.helloworld.demo.json.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -21,44 +20,41 @@ public class MyFirstServer {
                     System.out.println("Сокет запущен!");
                     byte[] inBuffer = new byte[2048];
                     int length;
+                    StringBuilder request = new StringBuilder();
                     while ((length = in.read(inBuffer)) > 0) {
-                        System.out.println(new String(inBuffer, 0, length, UTF_8));
+                        request.append(new String(inBuffer, 0, length, UTF_8));
                         if (in.available() <= 0) {
                             break;
                         }
                     }
+                    System.out.println(request);
+                    File file;
                     System.out.println("Связь  установлена!");
-
-                    serveConnection(socket);
+                    if (request.charAt(5) == ' ') {
+                        file = new File("C:/Users/user/Desktop/html/index.html");
+                    } else {
+                        String filePath = "C:/Users/user/Desktop/html/" + request.substring(5, request.indexOf(" ", 6));
+                        file = new File(filePath);
+                    }
+                    serveConnection(socket, file);
                 }
             }
         }
     }
 
-    public static void serveConnection(Socket socket) throws IOException {
-        try (OutputStream out = socket.getOutputStream()) {
-            System.out.println(readFile());
-            byte[] fileToBytes = readFile().getBytes(UTF_8);
+    public static void serveConnection(Socket socket, File file) throws IOException {
+        try (OutputStream out = socket.getOutputStream();
+             FileInputStream fis = new FileInputStream(file)) {
+            byte[] fileToBytes = fis.readAllBytes();
             String r = "HTTP/1.1 200 OK" +
                     "Content-Length: " + fileToBytes.length + "\n" +
                     "Content-Type: text/html";
-            String response = r + "\n\n" + readFile() + "\n";
+            String response = r + "\n\n";
             out.write(response.getBytes(UTF_8));
             System.out.println(response);
+            out.write(fileToBytes);
         }
-    }
 
 
-    public static String readFile() throws IOException {
-        File file = new File("C:/Users/user/Desktop/html/index.html");
-        byte[] buffer = new byte[2048];
-        int length;
-        StringBuilder htmlSb = new StringBuilder();
-        try (InputStream fin = new FileInputStream(file)) {
-            while ((length = fin.read(buffer)) > 0) {
-                htmlSb.append(new String(buffer, 0, length, StandardCharsets.UTF_8));
-            }
-        }
-        return htmlSb.toString();
     }
 }
