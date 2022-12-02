3c7d6a75f7f5c994348ca2c71d77cff94b204719
ZiynatI <zisomurodova@gmail.com>
2022-12-02 21:24:50 +0500

    Pair.java updated

--- src/org/helloworld/demo/linkedlist/Pair.java
+++ src/org/helloworld/demo/linkedlist/Pair.java
@@ -11,13 +11,14 @@ public class Pair<T, R> {
 
     @Override
     public String toString() {
-        return "(" + left.toString() + ":" + right.toString() + ")";
+        return "(" + (left == null ? null : left.toString()) + ":" + (right == null ? null : right.toString()) + ")";
     }
 
     @Override
     public boolean equals(Object that) {
         if (that instanceof Pair) {
             Pair<?, ?> that2 = (Pair<?, ?>) that;
+            assert this.left != null && this.right != null && that2.left != null && that2.right != null;
             return this.left.equals(that2.left) && this.right.equals(that2.right);
         }
         return false;
@@ -25,6 +26,6 @@ public class Pair<T, R> {
 
     @Override
     public int hashCode() {
-        return this.right.hashCode() * 549 + this.left.hashCode() * 987;
+        return (this.right == null ? 0 : this.right.hashCode()) * 549 + (this.right == null ? 0 : this.left.hashCode()) * 987;
     }
 }
