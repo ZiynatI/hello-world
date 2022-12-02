e91097bf045d7f3d3c67013a6f4157a02a83a382
ZiynatI <zisomurodova@gmail.com>
2022-12-02 17:43:47 +0500

    Pair.java created

---
+++ src/org/helloworld/demo/linkedlist/Pair.java
@@ -0,0 +1,11 @@
+package org.helloworld.demo.linkedlist;
+
+public class Pair<T> {
+    public final T LEFT;
+    public final T RIGHT;
+
+    public Pair(T LEFT, T RIGHT) {
+        this.LEFT = LEFT;
+        this.RIGHT = RIGHT;
+    }
+}
