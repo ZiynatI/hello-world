72987e2f59ac32e49984a1f659823ac4f2ef3754
ZiynatI <zisomurodova@gmail.com>
2022-12-14 23:19:26 +0500

    LambdaAsClass created

--- src/codewars/streamapi/FibonacciFunctional.java
+++ src/codewars/streamapi/FibonacciFunctional.java
@@ -4,8 +4,9 @@ import java.util.stream.IntStream;
 
 public class FibonacciFunctional {
     public static void main(String[] args) {
-        System.out.println(stream(0,1));
+        System.out.println(stream(0, 1));
     }
+
     //Write a function stream(int a, int b) -> IntStream that produces the Fibonacci sequence
     //where the first two elements are a and b. Here's the catch: you only get one semicolon!
     public static IntStream stream(int a, int b) {
@@ -13,8 +14,10 @@ public class FibonacciFunctional {
         return IntStream.concat(
                 IntStream.of(a, b),
                 IntStream.generate(() -> {
-                    return a+b;
+                    return a + b;
                 })
         );
     }
 }
+
+
