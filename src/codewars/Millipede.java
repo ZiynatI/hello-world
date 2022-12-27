4ca37368cfd917a83d48c0b03c6a3196f51f4c68
ZiynatI <zisomurodova@gmail.com>
2022-12-28 01:57:02 +0500

    Millipede created

---
+++ src/codewars/Millipede.java
@@ -0,0 +1,38 @@
+package codewars;
+
+public class Millipede {
+    //https://www.codewars.com/kata/6344701cd748a12b99c0dbc4
+    //The set of words is given.
+    //Words are joined if the last letter of one word and the first letter of another word are the same.
+    //Return true if all words of the set can be combined into one word.
+    //Each word can and must be used only once. Otherwise return false.
+    //Input
+    //Array of 3 to 7 words of random length. No capital letters.
+    //Example true
+    //Set: excavate, endure, desire, screen, theater, excess, night.
+    //Millipede: desirE EndurE ExcavatE ExcesS ScreeN NighT Theater.
+    //Example false
+    //Set: trade, pole, view, grave, ladder, mushroom, president.
+    //Millipede: presidenT Trade.
+    public static boolean check(String[] millipede) {
+        char lastChar = millipede[0].charAt(millipede[0].length() - 1);
+        millipede[0] = "";
+        for (int i = 1; i < millipede.length; i++) {
+            for (int j = 1; j < millipede.length; j++) {
+                if (!millipede[j].equals("")) {
+                    if (lastChar == millipede[j].charAt(0)) {
+                        lastChar = millipede[j].charAt(millipede[j].length() - 1);
+                        millipede[j] = "";
+                        break;
+                    }
+                }
+            }
+        }
+        for (String s : millipede) {
+            if (!s.equals("")) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
