2eff5fffb6ce95469e12ee764ea43cc39d96bf2f
ZiynatI <zisomurodova@gmail.com>
2022-12-30 19:09:11 +0500

    ThreadDemo updated

--- src/org/helloworld/demo/ThreadDemo.java
+++ src/org/helloworld/demo/ThreadDemo.java
@@ -12,15 +12,34 @@ public class ThreadDemo {
     }
 
     public static void main(String[] args) throws Exception {
-        new Thread(new MyNewRunnable()).start();
-        new Thread(new MyNewRunnable()).start();
-        new Thread(new MyNewRunnable()).start();
-        new Thread(new MyNewRunnable()).start();
-        new Thread(new MyNewRunnable()).start();
-        Thread.sleep(1000);
-        System.out.println(list.size());
 
-        new MyNewRunnable().run();
+        //        new Thread(new MyNewRunnable()).start();
+        //        new Thread(new MyNewRunnable()).start();
+        //        new Thread(new MyNewRunnable()).start();
+        //        new Thread(new MyNewRunnable()).start();
+        //        new Thread(new MyNewRunnable()).start();
+        //        Thread.sleep(1000);
+        //        new MyNewRunnable().run();
+        //        System.out.println(list.size());
+
+        Thread th = new Thread(() -> {
+            try {
+                while (!Thread.interrupted()) {
+                    //
+                }
+                Thread.currentThread().interrupt();
+                System.out.println("Interrupt status: " + Thread.interrupted());
+                System.out.println("Interrupt status: " + Thread.interrupted());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+        th.start();
+        Thread.sleep(1000);
+        System.out.println("Interrupting");
+        th.interrupt();
+        th.join();
+        System.out.println("Done!");
     }
 
     private static class MyNewRunnable implements Runnable {
