5011a91d087fed5703733c534190e9685917d2c1
ZiynatI <zisomurodova@gmail.com>
2023-01-16 12:48:09 +0500

    Sorting updated

--- src/org/helloworld/implementation/Sorting.java
+++ src/org/helloworld/implementation/Sorting.java
@@ -1,6 +1,17 @@
 package org.helloworld.implementation;
 
+import java.util.Arrays;
+
 public class Sorting {
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 2, 1, 3, 9, 0, 4, 6, 8, 7};
+        combSort(array);
+        System.out.println(Arrays.toString(array));
+        int[] array2 = new int[]{5, 2, 1, 3, 9, 0, 4, 6, 8, 7};
+        insertionSort(array2);
+        System.out.println(Arrays.toString(array2));
+    }
+
     public static void bubbleSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
@@ -15,7 +26,7 @@ public class Sorting {
 
     public static void bidirectionalBubbleSort(int[] array) {
         int begin = 0;
-        int end = array.length;
+        int end = array.length - 1;
         do {
             for (int i = begin; i < end; i++) {
                 if (array[i] > array[i + 1]) {
@@ -36,4 +47,44 @@ public class Sorting {
             begin++;
         } while (begin < end);
     }
+
+    public static void combSort(int[] array) {
+        int begin = 0;
+        int end = array.length - 1;
+        while (end > 0) {
+            for (int i = end; i < array.length; i++) {
+                if (array[begin] > array[i]) {
+                    int temp = array[i];
+                    array[i] = array[begin];
+                    array[begin] = temp;
+                }
+                begin++;
+            }
+            end--;
+            begin = 0;
+        }
+    }
+
+    public static void insertionSort(int[] array) {
+        int counter = 1;
+        while (counter < array.length) {
+            if (array[counter] < array[counter - 1]) {
+                for (int i = counter; i > 0; i--) {
+                    if (array[i] < array[i - 1]) {
+                        int temp = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = temp;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            counter++;
+        }
+    }
+
+    public static void quicksort(int[] array){
+
+    }
+    
 }
