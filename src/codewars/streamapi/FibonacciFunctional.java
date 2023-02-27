5972d677507716193fafad43eec877d453c77d5f
ZiynatI <zisomurodova@gmail.com>
2023-02-27 18:37:21 +0500

    FibonacciFunctional.java updated

--- src/codewars/streamapi/FibonacciFunctional.java
+++ src/codewars/streamapi/FibonacciFunctional.java
@@ -1,10 +1,7 @@
 package codewars.streamapi;
 
 public class FibonacciFunctional {
-    public static void main(String[] args) {
-        stream(0, 1).limit(100).forEach(System.out::println);
-    }
-
+    //https://www.codewars.com/kata/59e625bf3d09a7471d00020e
     //Write a function stream(int a, int b) -> IntStream that produces the Fibonacci sequence
     //where the first two elements are a and b. Here's the catch: you only get one semicolon!
     public static java.util.stream.IntStream stream(int a, int b) {
