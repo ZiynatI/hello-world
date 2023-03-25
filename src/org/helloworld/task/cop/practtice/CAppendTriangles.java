a49086c35ec7ac32422cb9966f840a05ea2d43fa
ZiynatII <zisomurodova@gmail.com>
2023-03-26 00:07:31 +0500

    CAppendTriangles solved

--- src/org/helloworld/task/cop/practtice/CAppendTriangles.java
+++ src/org/helloworld/task/cop/practtice/CAppendTriangles.java
@@ -45,7 +45,7 @@ import java.util.Scanner;
 public class CAppendTriangles {
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
-        int n = stdin.nextInt();
+        int n = Integer.parseInt(stdin.nextLine());
         while (n > 0) {
             findNumOfTTriangles(stdin);
             n--;
